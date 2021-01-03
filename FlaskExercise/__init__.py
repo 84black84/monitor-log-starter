f05cb274b94c44b2fb8133fa3e5ac34c13bdd136
Mavrogiannis, Christos <Christos.Mavrogiannis@ottobock.de>
2021-01-03 11:06:24 +0100

    Resolving excersice - first approach

--- FlaskExercise/__init__.py
+++ FlaskExercise/__init__.py
@@ -1,9 +1,34 @@
-import logging
 from flask import Flask
+import logging
+from flask.logging import default_handler
+# from logging.config import dictConfig
+
+# dictConfig({
+#     'version': 1,
+#     'formatters': {'default': {
+#         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+#     }},
+#     'handlers': {'wsgi': {
+#         'class': 'logging.StreamHandler',
+#         'stream': 'ext://flask.logging.wsgi_errors_stream',
+#         'formatter': 'default'
+#     }},
+#     'root': {
+#         'level': 'INFO',
+#         'handlers': ['wsgi']
+#     }
+# })
 
 app = Flask(__name__)
 wsgi_app = app.wsgi_app
-# TODO: Set the app's logger level to "warning"
-#   and any other necessary changes
+
+#  Set the app's logger level to "warning"
+#  and any other necessary changes
+
+app.logger = logging.getLogger(app.name)
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
 
 import FlaskExercise.views
--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -5,8 +5,16 @@ from FlaskExercise import app
 @app.route('/')
 def home():
     log = request.values.get('log_button')
-    # TODO: Appropriately log the different button presses
-    #   with the appropriate log level.
+    # Appropriately log the different button presses with the appropriate log level.
+    if log == "warning":
+        app.logger.info('Warning occurred')
+    elif log == "error":
+        app.logger.info('Error occurred')
+    elif log == "critical":
+        app.logger.critical("Critical error occurred")
+    else:    
+        app.logger.info("No issue \n")
+
     return render_template(
         'index.html',
         log=log
