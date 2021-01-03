224b5b87575a18fb8ee13207259b098b718347a7
Mavrogiannis, Christos <Christos.Mavrogiannis@ottobock.de>
2021-01-03 11:27:24 +0100

    Minor bug fixing

--- FlaskExercise/__init__.py
+++ FlaskExercise/__init__.py
@@ -1,23 +1,23 @@
 from flask import Flask
 import logging
 from flask.logging import default_handler
-# from logging.config import dictConfig
+from logging.config import dictConfig
 
-# dictConfig({
-#     'version': 1,
-#     'formatters': {'default': {
-#         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-#     }},
-#     'handlers': {'wsgi': {
-#         'class': 'logging.StreamHandler',
-#         'stream': 'ext://flask.logging.wsgi_errors_stream',
-#         'formatter': 'default'
-#     }},
-#     'root': {
-#         'level': 'INFO',
-#         'handlers': ['wsgi']
-#     }
-# })
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 wsgi_app = app.wsgi_app
@@ -26,7 +26,7 @@ wsgi_app = app.wsgi_app
 #  and any other necessary changes
 
 app.logger = logging.getLogger(app.name)
-app.logger.setLevel(logging.WARNING)
+# app.logger.setLevel(logging.WARNING)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
